141705311330d2ca109c74dcd5aa192c2bdd103e
Amboara13 <amboarafy13@gmail.com>
2025-09-17 20:15:15 +0300

    ...

---
+++ app/controllers/AnnoncesController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\controllers;
+use app\models\AdminModel;
+
+
+use Flight;
+
+class AnnoncesController {
+    public function AppelCreateAnnonce() {
+        Flight::render('accueilG');
+    }
+}
+
+?>
