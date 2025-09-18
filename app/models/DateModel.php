db79a2970c20e883161a7e5a0cf52610e81d74b4
Valisoaromuld <romualdrabenandrasana@gmail.com>
2025-09-18 13:15:39 +0300

    creation du modele DateModel

---
+++ app/models/DateModel.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\models;
+
+use Flight;
+use flight\Engine;
+use flight\database\PdoWrapper;
+use flight\debug\database\PdoQueryCapture;
+class DateModel{ 
+    private object $date;
+    public function __construct($id_departement, $duree_entretien) {
+
+    }
+}
