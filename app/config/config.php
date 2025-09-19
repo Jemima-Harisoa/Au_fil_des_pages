9b6aed778d9a490fe59fef2aadbd1dbbb661eb09
MUnderscore <rasolofoharilala.s.manoa@Gmail.com>
2025-09-19 10:29:51 +0300

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -70,11 +70,23 @@ if (Debugger::$showBar && php_sapi_name() !== 'cli') {
 return [
 	'database' => [
 		// uncomment the below 4 lines for mysql
-		 'host' => 'localhost',
-		 'port' => '5432',
-		 'dbname' => 'pages',
-		 'user' => 'postgres',
-		 'password' => 'manoa'
+		'driver'   => 'pgsql',
+		'host' => 'localhost',
+		'port' => '5432',
+		'dbname' => 'pages',
+		'user' => 'postgres',
+		'password' => 'manoa'
+
+		//  return [
+		// 	'database' => [
+		// 		'driver'   => 'pgsql',
+		// 		'host'     => 'localhost',
+		// 		'port'     => '5432', // le port par défaut de PostgreSQL
+		// 		'dbname'   => 'aufildespages',
+		// 		'user'     => 'postgres',
+		// 		'password' => 'postgres'
+		// 	],
+		
 
 		// uncomment the following line for sqlite
 		// 'file_path' => __DIR__ . $ds . '..' . $ds . 'database.sqlite'
