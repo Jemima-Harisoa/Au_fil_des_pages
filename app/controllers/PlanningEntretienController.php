f51671c3bad279fe5cabd142bf78e49eafe00707
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-09-22 02:50:26 +0300

    changement du nom de la fonction en PlanningEntretienController

--- app/controllers/PlanningEntretienController.php
+++ app/controllers/PlanningEntretienController.php
@@ -3,7 +3,7 @@ namespace app\controllers;
 use app\models\PlanningEntretienModel;
 use Flight;
 class PlanningEntretienController {
-    public function showAllPlanningEntretien() {
+    public function showPageEntretien() {
         Flight::render('planning_entretien');
     }
 }
