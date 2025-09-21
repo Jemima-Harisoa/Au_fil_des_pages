9e14404d9110d4586546be6e2a28c2354f98cc6d
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-09-21 05:32:10 +0300

    modification de db() en db dans la fonction delete de la classe DispoinibiliteEntretienModel

--- app/models/DisponibiliteEntretienModel.php
+++ app/models/DisponibiliteEntretienModel.php
@@ -68,7 +68,7 @@ class DisponibiliteEntretienModel {
     }
     public function getTempsDisponiblesEntretien($idResponsable){
         $query = "SELECT * FROM disponibilite_entretien where id_responsable = :id_responsable ORDER by jour asc";
-        $db = $this->db();
+        $db = $this->db;
         try{
             if($idResponsable== 0 || $idResponsable == null){
                 throw new \Exception("l'id du Responsable doit etre positive et non nul");
@@ -79,5 +79,54 @@ class DisponibiliteEntretienModel {
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
+    }
+    // fonction qui sert a trouver la date de disponibilite d'entretien ouvrable
+    public static function getDateNonFerieProche($dateHeure,$listeDisponibiliteEntretien){
+        $indiceActuel = 0;
+        $diffJour = 0;
+        $chiffreJour = DateModel::getJourChiffreDate($dateHeure);
+        for($i = 0 ; $i < count($listeDisponibiliteEntretien);$i++){
+            if($listeDisponibiliteEntretien[$i]["jour"] ==$chiffreJour){
+                $indiceActuel = $i;
+                break;
+            }
+        }
+        while(JourFerieModel::estJourFerie($dateHeure)){
+            if($indiceActuel==count($listeDisponibiliteEntretien) -1){
+                $diffJour = ($listeDisponibiliteEntretien[0]["jour"]+7)-$listeDisponibiliteEntretien[$indiceActuel];
+                $indiceActuel = 0;
+            }
+            else{
+                $diffJour = $listeDisponibiliteEntretien[$indiceActuel+1]-$listeDisponibiliteEntretien[$indiceActuel];
+            }
+            $dateHeure = DateModel::ajouterJours($dateHeure,$diffJour);
+            $indiceActuel++;
+        }
+        return $dateHeure;
+    }
+    public static function checkDateDisponible($dateHeure,$listeDisponibiliteEntretien){
+        $diffJour = 0;
+        for($i=0; $i<count($listeDisponibiliteEntretien);$i++){
+            if($listeDisponibiliteEntretien[$i]["jour"] == DateModel::getJourChiffreDate($dateHeure)){
+                if(DateModel::recupererHeure($dateHeure) <= $listeDisponibiliteEntretien[$i]["heure_fin"]){
+                    return DisponibiliteEntretienModel::getDateNonFerieProche($dateHeure,$listeDisponibiliteEntretien);
+                }
+                else{
+                    switch($i ){
+                        case count($listeDisponibiliteEntretien)-1:
+                            $diffJour = ($listeDisponibiliteEntretien[0]+7)-$listeDisponibiliteEntretien[$i]["jour"];
+                            break;
+                        default:
+                            error_log("diffJour: ".$listeDisponibiliteEntretien[$i]["jour"]);
+                            error_log("diffJour:".$diffJour);
+                            $diffJour = $listeDisponibiliteEntretien[$i+1]["jour"]-$listeDisponibiliteEntretien[$i]["jour"];
+                            break;
+                    }
+                    $dateHeure = DateModel::ajouterJours($dateHeure,$diffJour);
+                    DateModel::changerHeure($dateHeure,new \DateTime($listeDisponibiliteEntretien[$i]["heure_debut"]));
+                }
+            }
+        }
+        return $dateHeure;
     }
 }
