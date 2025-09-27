9fe809946a90608571671b6bf4edea17b23a98fe
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-09-27 12:40:29 +0300

    ajout du model et du controleur de Connexion

---
+++ app/controllers/ConnexionController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace app\controllers;
+
+session_start();
+
+use Flight;
+use app\models\ConnexionModel;
+use app\models\AdminModel;
+
+use app\models\MessagerieModel;
+
+class ConnexionController {
+
+	public function __construct() {
+
+	}
+
+    
+    // UTILISATEURS SIMPLES
+    public static function AppelLoginU()
+    {
+		Flight::render('connexionU',null);
+    }
+
+	public function VerificationConnectionU()
+    {
+        $p = new ConnexionModel(Flight::db());
+
+        $messagerieModel = new messagerieModel(Flight::db());
+        
+            $Nom = $_POST['Nom'];
+            $mdp = $_POST['mdp'];
+        $v = $p->verifierUtilisateur($Nom, $mdp);
+        if($v == true)
+        {
+            $_SESSION['utilisateur']  = $p->getUtilisateur($Nom, $mdp);
+            $_SESSION['messagerie'] = $messagerieModel->getTitresConversationsU($_SESSION['utilisateur']['id_utilisateur']);  
+            $_SESSION['nbNonLus'] = $messagerieModel->countNouveauxMessagesU($_SESSION['utilisateur']['id_utilisateur']);
+            
+            Flight::render('accueilU',null);
+
+        }
+        else {
+            $mess = "Verifier votre mot de passe ou votre nom d'utilisateur";
+            Flight::render('connexionU', ['mess' => $mess]);
+        }
+    }
+
+    public function InscrireU()
+    {
+        $p = new ConnexionModel(Flight::db());
+        
+            $Nom = $_POST['Nom'];
+            $mdp = $_POST['mdp'];
+        $v = $p->inscrireUtilisateur($Nom, $mdp);
+        
+        if($v == true)
+        {
+            $_SESSION['utilisateur']  = $p->getUtilisateur($Nom, $mdp);     
+            Flight::render('accueilU',null);
+        
+        }
+        else {
+            $mess = "Erreur lors de l'inscription , Verifiez votre connexion internet et reessayez";
+            Flight::render('connexionU', ['mess' => $mess]);
+        }
+    }
+
+    // ADMINS
+    public static function AppelLoginA()
+    {
+		Flight::render('connexionA',null);
+    }
+
+	public function VerificationConnectionA()
+    {
+        //tezitra
+        $idGestion = 1;
+        $p = new ConnexionModel(Flight::db());
+        $AdminModel = new AdminModel(Flight::db());
+
+        $messagerieModel = new MessagerieModel(Flight::db());
+
+
+        
+            $Nom = $_POST['Nom'];
+            $mdp = $_POST['mdp'];
+
+
+        $v = $p->verifierAdmin($Nom, $mdp);
+        if($v == true)
+        {
+            $_SESSION['admin']  = $p->getAdmin($Nom, $mdp); 
+            $_SESSION['departement']  = $p-> getDepartementAdmin($_SESSION['admin']['id_admin']);     
+            $_SESSION['infoAdmin'] = $AdminModel -> getDetailsPersoAdmin($_SESSION['admin']['id_admin']);
+            $_SESSION['messagerie'] = $messagerieModel->getTitresConversationsA();  
+            $_SESSION['nbNonLus'] = $messagerieModel->countNouveauxMessagesA();
+
+            if($_SESSION['departement']['id_departement'] ==  $idGestion  )
+            {
+                Flight::render('accueilG',null);    
+            }
+            else{
+                Flight::render('accueilA',null);
+            }
+
+
+        }
+        else {
+            $mess = "Verifier votre mot de passe ou votre nom d'utilisateur admin";
+            Flight::render('connexionA', ['mess' => $mess]);
+        }
+    }
+    
+    public function deconnexion() {
+        $model = new ConnexionModel(Flight::db());
+
+       
+        if(isset($_SESSION['admin']))
+        {
+            $model->deconnexion();
+            Flight::redirect('/admin');
+        }else{
+                    $model->deconnexion();
+        Flight::redirect('/');
+        }
+
+    }
+
+    
+}
