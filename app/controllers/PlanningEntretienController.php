820f010ea6996452e9b383993a00468fa78bf6f7
Valisoaromuald <romualdrabenandrasana@gmail.com>
2025-09-28 07:24:29 +0300

    utilisation de $_Session["idAdmin"]

--- app/controllers/PlanningEntretienController.php
+++ app/controllers/PlanningEntretienController.php
@@ -5,6 +5,8 @@ use Flight;
 class PlanningEntretienController {
     public function showPageEntretien() {
         $candidats = Flight::testModel()->getCandidatsAvecSuccesTest(4);
+        $_SESSION["IdAdmin"] = 3;
+        $candidats = Flight::responsableEntretienModel()->getListeEntretiensInListeCandidats($candidats,3);
         $planningEntretien = Flight::planningEntretienModel();
         $allVerified = $planningEntretien-> checkCandidatsInEntretien($candidats);
         $entretiens = $planningEntretien->all();
